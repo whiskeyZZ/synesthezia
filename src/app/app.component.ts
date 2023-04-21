a7a73bcc3e26190b67b178bc5560f9bc8204b0c7
whiskeyZZ <julian.ortmann@gmx.de>
2023-04-21 10:28:17 -0400

    added upload function, createt info-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { InfoDialogComponent } from './info-dialog/info-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'synesthezia';
-  
+  context: AudioContext;
+  audioFiles: File[] = [];
+  fileList: FileList | null = null;
+
+  constructor(public dialog: MatDialog) {
+    this.context = new AudioContext();
+  }
+
+  openInfo(): void {
+    const dialogRef = this.dialog.open(InfoDialogComponent, {
+      width: '50%',
+    });
+  }
+
+  openFileManager() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.mp3';
+    input.onchange = (event) => {
+      if (event.target != null) {
+        this.fileList = (event.target as HTMLInputElement).files;
+      }
+      if (this.fileList != null) {
+        this.audioFiles.push(this.fileList[0]);
+      }
+    };
+    input.click();
+  }
+
+  showAudioFiles() {
+    this.audioFiles.forEach((element) => {
+      console.log(element.name);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { InfoDialogComponent } from './info-dialog/info-dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MatIconModule],
+  declarations: [AppComponent, InfoDialogComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
