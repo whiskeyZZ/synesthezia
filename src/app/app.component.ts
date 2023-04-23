939e55ef0dadaf260bfa136e29ba02f16f6660e9
whiskeyZZ <julian.ortmann@gmx.de>
2023-04-23 14:16:10 -0400

    pause function + cutoff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
+import { HttpContext } from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { InfoDialogComponent } from './info-dialog/info-dialog.component';
@@ -10,18 +11,27 @@ import { InfoDialogComponent } from './info-dialog/info-dialog.component';
 })
 export class AppComponent {
   title = 'synesthezia';
-  context: AudioContext;
+  context: AudioContext = new AudioContext();
   buffer: AudioBuffer | null = null;
+  source: AudioBufferSourceNode | null = null;
+
+  filter = this.context.createBiquadFilter();
 
   audioFiles: File[] = [];
   fileList: FileList | null = null;
   currentAudio: File | null = null;
 
+  isPlaying: boolean = false;
+
   @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
+  @ViewChild('cutoffCursor') cutoffCursor!: ElementRef;
+  @ViewChild('cutoffField') cutoffField!: ElementRef;
+
+  isCutoff: boolean = true;
+  cutoff: number = 50;
+  qFactor: number = 2;
 
   constructor(public dialog: MatDialog) {
-    this.context = new AudioContext();
-
     this.addAudioFiles();
   }
 
@@ -48,7 +58,7 @@ export class AppComponent {
 
   addAudioFiles() {
     const soundPath = 'assets/sounds/';
-    const soundFileNames = ['bell.mp3'];
+    const soundFileNames = ['bell.mp3', 'rain.mp3', 'airplane.mp3'];
 
     soundFileNames.forEach((fileName) => {
       fetch(soundPath + fileName)
@@ -79,9 +89,50 @@ export class AppComponent {
   }
 
   play() {
-    const source = this.context.createBufferSource();
-    source.buffer = this.buffer;
-    source.connect(this.context.destination);
-    source.start(0);
+    if (this.isPlaying == false) {
+      this.isPlaying = true;
+      this.source = this.context.createBufferSource();
+      this.source.buffer = this.buffer;
+      if (this.isCutoff) {
+        this.filter.type = 'bandpass';
+        this.filter.frequency.value = this.cutoff;
+        this.filter.Q.value = this.qFactor;
+        this.source.connect(this.filter).connect(this.context.destination);
+      } else {
+        this.source.connect(this.context.destination);
+      }
+      this.source.start();
+    } else {
+      this.isPlaying = false;
+      if (this.source != null) {
+        this.source.stop();
+      }
+    }
+  }
+
+  calcCutoff() {
+    const rect = this.cutoffField.nativeElement.getBoundingClientRect();
+    const l = rect.left;
+    const r = rect.right;
+    const rectCursor = this.cutoffCursor.nativeElement.getBoundingClientRect();
+    const c = rectCursor.left;
+    const x = (c - l) / (r - l);
+    this.cutoff = 14000 * x;
+    if (this.cutoff < 20) {
+      this.cutoff = 20;
+    }
+
+    const t = rect.top;
+    const d = rect.bottom;
+    const cTop = rectCursor.top;
+    const y = (t - cTop) / (t - d);
+    this.qFactor = 20 * y;
+    if (this.qFactor < 0.5) {
+      this.qFactor = 0.5;
+    }
+
+    this.filter.frequency.value = this.cutoff;
+    this.filter.Q.value = this.qFactor;
+    console.log(this.qFactor);
   }
 }
