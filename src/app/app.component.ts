bf07306248acdde3f1896a3607f8d1106bd98de8
whiskeyZZ <julian.ortmann@gmx.de>
2023-05-12 08:48:48 -0400

    reverb and effect chain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { DomSanitizer } from '@angular/platform-browser';
 import { InfoDialogComponent } from './info-dialog/info-dialog.component';
-import 'http://reverbjs.org/reverb.js';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +14,7 @@ export class AppComponent {
   title = 'synesthezia';
   context: AudioContext = new AudioContext();
   buffer: AudioBuffer | null = null;
+  impulseBuffer: AudioBuffer | null = null;
   source: AudioBufferSourceNode | null = null;
 
   filterLow = this.context.createBiquadFilter();
@@ -36,6 +36,7 @@ export class AppComponent {
   audioFiles: File[] = [];
   fileList: FileList | null = null;
   currentAudio: File | null = null;
+  impulseFiles: File[] = [];
 
   @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
   @ViewChild('cutoffCursor') cutoffCursor!: ElementRef;
@@ -43,20 +44,24 @@ export class AppComponent {
   @ViewChild('cutoffTitle') cutoffTitle!: ElementRef;
   @ViewChild('delayCursor') delayCursor!: ElementRef;
   @ViewChild('delayField') delayField!: ElementRef;
+  @ViewChild('delayTitle') delayTitle!: ElementRef;
   @ViewChild('reverbCursor') reverbCursor!: ElementRef;
   @ViewChild('reverbField') reverbField!: ElementRef;
+  @ViewChild('reverbTitle') reverbTitle!: ElementRef;
 
   graph: CanvasRenderingContext2D | null = null;
   graphMirror: CanvasRenderingContext2D | null = null;
 
   isPlaying: boolean = false;
   isCutoff: boolean = false;
-  isDelay: boolean = true;
+  isDelay: boolean = false;
+  isReverb: boolean = false;
   cutoff: number = 50;
   cutoffHigh: number = 20;
   delayTime: number = 1;
   delayFeedback: number = 0.1;
   qFactor: number = 2;
+  chain: string[] = [];
 
   animationReq: number = 0;
 
@@ -66,6 +71,7 @@ export class AppComponent {
 
   constructor(public dialog: MatDialog, private domSanitizer: DomSanitizer) {
     this.addAudioFiles();
+    this.loadImpulseFiles();
 
     this.analyser.fftSize = 2048;
     const bufferLength = this.analyser.frequencyBinCount;
@@ -90,7 +96,7 @@ export class AppComponent {
   openFileManager() {
     const input = document.createElement('input');
     input.type = 'file';
-    input.accept = '.mp3';
+    input.accept = '.mp3,.wav';
     input.onchange = (event) => {
       if (event.target != null) {
         this.fileList = (event.target as HTMLInputElement).files;
@@ -130,6 +136,36 @@ export class AppComponent {
     }
   }
 
+  loadImpulseFiles() {
+    const soundPath = 'assets/impulses/';
+    const soundFileNames = ['punch.wav'];
+
+    soundFileNames.forEach((fileName) => {
+      fetch(soundPath + fileName)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const file = new File([blob], fileName);
+          this.impulseFiles.push(file);
+        });
+    });
+  }
+
+  getImpulseBuffer() {
+    console.log('Test234');
+    console.log(this.impulseFiles);
+    const reader = new FileReader();
+    reader.onload = () => {
+      console.log('test333');
+      const arrayBuffer = reader.result as ArrayBuffer;
+      this.context.decodeAudioData(arrayBuffer, (audioBuffer) => {
+        this.impulseBuffer = audioBuffer;
+      });
+    };
+    if (this.impulseFiles[0] != null) {
+      reader.readAsArrayBuffer(this.impulseFiles[0]);
+    }
+  }
+
   openMenu() {
     this.trigger.openMenu();
   }
@@ -152,46 +188,103 @@ export class AppComponent {
   setEffects() {
     this.source = this.context.createBufferSource();
     this.source.buffer = this.buffer;
-    this.setCutoff(this.source);
+    /*this.setCutoff(this.source);
     this.setDelay(this.source);
-    this.setReverb();
+    this.setReverb();*/
+    if (this.chain.length > 0) {
+      if (this.chain[0] == 'cutoff') {
+        this.setCutoff(this.source, 0);
+      }
+      if (this.chain[0] == 'delay') {
+        this.setDelay(this.source, 0);
+      }
+      if (this.chain[0] == 'reverb') {
+        this.setReverb(this.source, 0);
+      }
+    } else {
+      this.source.connect(this.analyser);
+    }
+    if (this.chain.length > 1) {
+      if (this.chain[1] == 'cutoff') {
+        this.setCutoff(this.source, 1);
+      }
+      if (this.chain[1] == 'delay') {
+        this.setDelay(this.source, 1);
+      }
+      if (this.chain[1] == 'reverb') {
+        this.setReverb(this.source, 1);
+      }
+    }
+    if (this.chain.length > 2) {
+      if (this.chain[2] == 'cutoff') {
+        this.setCutoff(this.source, 2);
+      }
+      if (this.chain[2] == 'delay') {
+        this.setDelay(this.source, 2);
+      }
+      if (this.chain[2] == 'reverb') {
+        this.setReverb(this.source, 2);
+      }
+    }
     this.analyser.connect(this.context.destination);
     this.context.resume();
     this.source.start();
     this.draw();
   }
 
-  setCutoff(source: AudioBufferSourceNode) {
+  setCutoff(source: AudioBufferSourceNode, position: number) {
     if (this.isCutoff) {
       this.filterLow.type = 'lowpass';
       this.filterLow.frequency.value = this.cutoff;
       this.filterLow.Q.value = this.qFactor;
-      source.connect(this.filterLow);
+      if (position == 0) {
+        source.connect(this.filterLow);
+      } else {
+        if (this.chain[position - 1] == 'delay') {
+          this.delay.connect(this.filterLow);
+        }
+        if (this.chain[position - 1] == 'reverb') {
+          this.reverb.connect(this.filterLow);
+        }
+      }
       this.filterHigh.type = 'highpass';
       this.filterHigh.frequency.value = this.cutoffHigh;
       this.filterHigh.Q.value = this.qFactor;
       this.filterLow.connect(this.filterHigh);
-      if (!this.isDelay) {
+      if (this.chain[this.chain.length - 1] == 'cutoff') {
         this.filterHigh.connect(this.analyser);
       }
     }
   }
 
-  setDelay(source: AudioBufferSourceNode) {
+  setDelay(source: AudioBufferSourceNode, position: number) {
     this.delay.delayTime.value = this.delayTime;
     this.feedback.gain.value = this.delayFeedback;
-    if (this.isCutoff) {
+    if (position == 0) {
+      source.connect(this.delay);
+    } else {
+      if (this.chain[position - 1] == 'cutoff') {
+        this.filterHigh.connect(this.delay);
+      }
+      if (this.chain[position - 1] == 'reverb') {
+        this.reverb.connect(this.delay);
+      }
+    }
+    /*if (this.isCutoff) {
       this.filterHigh.connect(this.delay);
     }
     if (!this.isCutoff) {
       source.connect(this.delay);
-    }
+    }*/
     this.delay.connect(this.feedback);
     this.feedback.connect(this.delay);
+    if (this.chain[this.chain.length - 1] == 'delay') {
+      this.delay.connect(this.analyser);
+    }
     //this.delay.connect(this.analyser);
   }
 
-  setReverb() {
+  setReverb(source: AudioBufferSourceNode, position: number) {
     this.reverbDelay.delayTime.setValueAtTime(0.03, this.context.currentTime);
 
     for (let i = 2; i > 0; i--) {
@@ -207,19 +300,35 @@ export class AppComponent {
       );
     });
 
-    this.multitap[this.multitap.length - 1].connect(this.multitapGain);
+    /*this.multitap[this.multitap.length - 1].connect(this.multitapGain);
     this.multitapGain.gain.value = 0.5;
-    this.multitapGain.connect(this.analyser);
+    this.multitapGain.connect(this.analyser);*/
 
-    this.dryGain.gain.value = 0.2;
-    this.wetGain.gain.value = 1.0;
-
-    this.delay.connect(this.dryGain);
-    this.dryGain.connect(this.reverbDelay);
-    this.dryGain.connect(this.multitap[0]);
-    this.reverbDelay.connect(this.reverb);
+    this.dryGain.gain.value = 0.1;
+    this.wetGain.gain.value = 0.9;
+    if (position == 0) {
+      source.connect(this.reverb);
+      source.connect(this.dryGain);
+    } else {
+      if (this.chain[position - 1] == 'cutoff') {
+        this.filterHigh.connect(this.reverb);
+        this.filterHigh.connect(this.dryGain);
+      }
+      if (this.chain[position - 1] == 'delay') {
+        this.delay.connect(this.reverb);
+        this.delay.connect(this.dryGain);
+      }
+    }
+    this.getImpulseBuffer();
+    this.reverb.buffer = this.impulseBuffer;
     this.reverb.connect(this.wetGain);
-    this.wetGain.connect(this.analyser);
+
+    if (this.chain[this.chain.length - 1] == 'reverb') {
+      this.dryGain.connect(this.analyser);
+      this.wetGain.connect(this.analyser);
+    }
+
+    console.log(this.chain);
   }
 
   calcCutoff() {
@@ -276,12 +385,13 @@ export class AppComponent {
     const d = rect.bottom;
     const rectCursor = this.reverbCursor.nativeElement.getBoundingClientRect();
     const c = rectCursor.left;
-    let x = ((c - l) / (r - l)) * 5;
-    if (x < 1) {
+    let x = (c - l) / (r - l);
+    /*if (x < 1) {
       x = 1;
-    }
+    }*/
 
-    this.reverbDelay.delayTime.value = x;
+    this.dryGain.gain.value = 1.0 - x;
+    this.wetGain.gain.value = x;
   }
 
   draw() {
@@ -404,9 +514,40 @@ export class AppComponent {
     if (this.isCutoff == false) {
       this.isCutoff = true;
       this.cutoffTitle.nativeElement.style.opacity = '1.0';
+      this.chain.push('cutoff');
     } else {
       this.isCutoff = false;
       this.cutoffTitle.nativeElement.style.opacity = '0.5';
+      var index = this.chain.indexOf('cutoff');
+      this.chain.splice(index, 1);
+    }
+  }
+
+  activateDelay() {
+    if (this.isDelay == false) {
+      this.isDelay = true;
+      this.delayTitle.nativeElement.style.opacity = '1.0';
+      this.chain.push('delay');
+      console.log(this.chain);
+    } else {
+      this.isDelay = false;
+      this.delayTitle.nativeElement.style.opacity = '0.5';
+      var index = this.chain.indexOf('delay');
+      this.chain.splice(index, 1);
+      console.log(this.chain);
+    }
+  }
+
+  activateReverb() {
+    if (this.isReverb == false) {
+      this.isReverb = true;
+      this.reverbTitle.nativeElement.style.opacity = '1.0';
+      this.chain.push('reverb');
+    } else {
+      this.isReverb = false;
+      this.reverbTitle.nativeElement.style.opacity = '0.5';
+      var index = this.chain.indexOf('reverb');
+      this.chain.splice(index, 1);
     }
   }
 }
